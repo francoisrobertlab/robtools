739201774c619d55eeafcd1d5479c9e3e2c412a0
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-09 09:37:37 -0400

    Simplifies configuration script.

--- install/configure.sh
+++ install/configure.sh
@@ -1,27 +1,40 @@
-EMAIL=$1
-if [[ ! "$EMAIL" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]
-then
-    echo "You must supply your email address as the first argument"
-    exit 1
+#!/bin/bash
+
+if [ "$1" == "clean" ] ; then
+    echo "Removing changes made to .bash_profile"
+    if ! grep -Fq "source .def-robert-addons" ~/.bash_profile ; then
+        INDEX=$(grep -n "source .def-robert-addons" ~/.bash_profile | cut -d: -f1)
+        sed -i "$((INDEX-1)),$((INDEX+2))d" ~/.bash_profile
+    fi
+    if [ -f ~/.def-robert-addons ] ; then
+        rm ~/.def-robert-addons
+    fi
+    exit 0
 fi
 
-if grep -Fq "JOB_MAIL" ~/.bash_profile ; then
-  echo "Email address environment variables present in .bash_profile"
-else
-  echo "Adding email address to environment variables in .bash_profile"
-  echo "export JOB_MAIL=$EMAIL" >> ~/.bash_profile
-  echo "" >> ~/.bash_profile
+# Remove direct configuration of robert lab modules, if present.
+if grep -Fq "ROBERTF_MODULES_DIR=" ~/.bash_profile ; then
+    echo "Removing Robert Lab modules from .bash_profile"
+    INDEX=$(grep -n "ROBERTF_MODULES_DIR=" ~/.bash_profile | cut -d: -f1)
+    sed -i "$((INDEX-1)),$((INDEX+4))d" ~/.bash_profile
 fi
 
-if grep -Fq "ROBERTF_MODULES_DIR" ~/.bash_profile ; then
-  echo "Modules directory present in .bash_profile"
-else
-  echo "Adding modules directory to .bash_profile"
-  echo "## Robert Lab Modules ##" >> ~/.bash_profile
-  echo "ROBERTF_MODULES_DIR=~/projects/def-robertf/modules" >> ~/.bash_profile
-  echo 'if [ -d "$ROBERTF_MODULES_DIR" ]; then' >> ~/.bash_profile
-  echo '  module use $ROBERTF_MODULES_DIR' >> ~/.bash_profile
-  echo 'fi' >> ~/.bash_profile
-  echo "" >> ~/.bash_profile
+# Source .def-robert-addons file on login.
+if ! grep -Fq "source .def-robert-addons" ~/.bash_profile ; then
+    echo "Adding Robert Lab addons"
+    echo 'if [ -f .def-robert-addons ]; then' >> ~/.bash_profile
+    echo '  source .def-robert-addons' >> ~/.bash_profile
+    echo 'fi' >> ~/.bash_profile
+    echo "" >> ~/.bash_profile
 fi
 
+# Create .def-robert-addons file to alow loading of robert modules.
+if [ -f ~/.def-robert-addons ] ; then
+    rm ~/.def-robert-addons
+fi
+echo "## Robert Lab Modules ##" >> ~/.def-robert-addons
+echo "MODULES_DIR=~/projects/def-robertf/modules" >> ~/.def-robert-addons
+echo 'if [ -d "$MODULES_DIR" ]; then' >> ~/.def-robert-addons
+echo '  module use $MODULES_DIR' >> ~/.def-robert-addons
+echo 'fi' >> ~/.def-robert-addons
+echo "" >> ~/.def-robert-addons
