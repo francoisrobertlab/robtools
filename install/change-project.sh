66922697ba100ff3e839c0f82c870ce8ccc63f0e
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-09 12:26:26 -0400

    Validates that the project name exists in change-project.sh.

--- install/change-project.sh
+++ install/change-project.sh
@@ -5,5 +5,9 @@ if [ -z "$PROJECT" ] ; then
     echo "You must supply the project name as the first argument"
     exit 1
 fi
+if [ ! -d "$HOME/projects/$PROJECT" ] ; then
+    echo "Project $PROJECT not present in your projects folder"
+    exit 1
+fi
 
 find . -maxdepth 1 -type f -exec sed -i "s/def-robertf/$PROJECT/g" {} \;
