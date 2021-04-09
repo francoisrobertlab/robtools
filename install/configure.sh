59cd7a9511a50652e6a4ca943be2df319a18a8ff
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-09 11:49:00 -0400

    Fixes addon filename in configure.sh

--- install/configure.sh
+++ install/configure.sh
@@ -2,12 +2,12 @@
 
 if [ "$1" == "clean" ] ; then
     echo "Removing changes made to .bash_profile"
-    if ! grep -Fq "source .def-robert-addons" ~/.bash_profile ; then
-        INDEX=$(grep -n "source .def-robert-addons" ~/.bash_profile | cut -d: -f1)
+    if ! grep -Fq "source .def-robertf-addons" ~/.bash_profile ; then
+        INDEX=$(grep -n "source .def-robertf-addons" ~/.bash_profile | cut -d: -f1)
         sed -i "$((INDEX-1)),$((INDEX+2))d" ~/.bash_profile
     fi
-    if [ -f ~/.def-robert-addons ] ; then
-        rm ~/.def-robert-addons
+    if [ -f ~/.def-robertf-addons ] ; then
+        rm ~/.def-robertf-addons
     fi
     exit 0
 fi
@@ -19,22 +19,22 @@ if grep -Fq "ROBERTF_MODULES_DIR=" ~/.bash_profile ; then
     sed -i "$((INDEX-1)),$((INDEX+4))d" ~/.bash_profile
 fi
 
-# Source .def-robert-addons file on login.
-if ! grep -Fq "source .def-robert-addons" ~/.bash_profile ; then
+# Source .def-robertf-addons file on login.
+if ! grep -Fq "source .def-robertf-addons" ~/.bash_profile ; then
     echo "Adding Robert Lab addons"
-    echo 'if [ -f .def-robert-addons ]; then' >> ~/.bash_profile
-    echo '  source .def-robert-addons' >> ~/.bash_profile
+    echo 'if [ -f .def-robertf-addons ]; then' >> ~/.bash_profile
+    echo '  source .def-robertf-addons' >> ~/.bash_profile
     echo 'fi' >> ~/.bash_profile
     echo "" >> ~/.bash_profile
 fi
 
-# Create .def-robert-addons file to alow loading of robert modules.
-if [ -f ~/.def-robert-addons ] ; then
-    rm ~/.def-robert-addons
+# Create .def-robertf-addons file to alow loading of robert modules.
+if [ -f ~/.def-robertf-addons ] ; then
+    rm ~/.def-robertf-addons
 fi
-echo "## Robert Lab Modules ##" >> ~/.def-robert-addons
-echo "MODULES_DIR=~/projects/def-robertf/modules" >> ~/.def-robert-addons
-echo 'if [ -d "$MODULES_DIR" ]; then' >> ~/.def-robert-addons
-echo '  module use $MODULES_DIR' >> ~/.def-robert-addons
-echo 'fi' >> ~/.def-robert-addons
-echo "" >> ~/.def-robert-addons
+echo "## Robert Lab Modules ##" >> ~/.def-robertf-addons
+echo "MODULES_DIR=~/projects/def-robertf/modules" >> ~/.def-robertf-addons
+echo 'if [ -d "$MODULES_DIR" ]; then' >> ~/.def-robertf-addons
+echo '  module use $MODULES_DIR' >> ~/.def-robertf-addons
+echo 'fi' >> ~/.def-robertf-addons
+echo "" >> ~/.def-robertf-addons
