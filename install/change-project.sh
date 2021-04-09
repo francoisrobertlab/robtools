da2cfff613c17fc1693454fa1f742a2dc9ce7337
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-09 12:27:27 -0400

    Change default account in bash scripts in change-project.sh.

--- install/change-project.sh
+++ install/change-project.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+FOLDER_NAME=${PWD##*/}
+if [ ! "$FOLDER_NAME" == 'robtools' ] ; then
+    echo "You must run change-project.sh from robtools base folder which is expected to be called 'robtools'"
+    exit 1
+fi
+
 PROJECT=$1
 if [ -z "$PROJECT" ] ; then
     echo "You must supply the project name as the first argument"
@@ -10,4 +16,5 @@ if [ ! -d "$HOME/projects/$PROJECT" ] ; then
     exit 1
 fi
 
-find . -maxdepth 1 -type f -exec sed -i "s/def-robertf/$PROJECT/g" {} \;
+find install -maxdepth 1 -type f -exec sed -i "s/def-robertf/$PROJECT/g" {} \;
+find bash -maxdepth 1 -type f -exec sed -i "s/def-robertf/$PROJECT/g" {} \;
