e75a1276f97338a4db04b4cf93a1a40b9a0c9833
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-11-29 09:30:06 -0500

    PrintSample can print replicates of a dataset in addition to dataset's name.

--- robtools/PrintSample.py
+++ robtools/PrintSample.py
@@ -7,20 +7,25 @@ from robtools.txt import Parser
 @click.command()
 @click.option('--samples', '-s', type=click.Path(exists=True), default='samples.txt', show_default=True,
               help='Sample names listed one sample name by line.')
+@click.option('--replicates/--hide-replicates', '-p/-u', default=False, show_default=True,
+              help='Show all columns (replicates for dataset file) with additional suffix if specified')
 @click.option('--suffix', default='', show_default=True,
               help='Suffix added to sample name.')
 @click.option('--index', '-i', type=int, required=True,
               help='Index of sample to process in samples file.')
-def printsample(samples, suffix, index):
+def printsample(samples, replicates, suffix, index):
     '''Print's sample's name at index of samples file.'''
-    print_sample(samples, suffix, index)
+    print_sample(samples, replicates, suffix, index)
 
 
-def print_sample(samples='samples.txt', suffix='', index=0):
+def print_sample(samples='samples.txt', replicates=False, suffix='', index=0):
     '''Print's sample's name to output'''
-    sample_names = Parser.first(samples)
-    sample_names = [sample_names[index]]
-    print (sample_names[0] + suffix)
+    sample_names = Parser.columns(samples)
+    sample_names = sample_names[index]
+    if not replicates:
+        sample_names = sample_names[:1]
+    sample_names = [sample + suffix for sample in sample_names]
+    print("\t".join(sample_names))
 
 
 if __name__ == '__main__':
--- tests/robtools/test_PrintSample.py
+++ tests/robtools/test_PrintSample.py
@@ -26,18 +26,18 @@ def test_printsample(testdir, mock_testclass):
     runner = CliRunner()
     result = runner.invoke(ps.printsample, ['-s', samples, '-i', index])
     assert result.exit_code == 0
-    ps.print_sample.assert_called_once_with(samples, '', index)
+    ps.print_sample.assert_called_once_with(samples, False, '', index)
 
 
-def test_printsample_suffix(testdir, mock_testclass):
+def test_printsample_parameters(testdir, mock_testclass):
     samples = Path(__file__).parent.joinpath('samples.txt')
     suffix = '-dedup'
     index = 0
     ps.print_sample = MagicMock()
     runner = CliRunner()
-    result = runner.invoke(ps.printsample, ['-s', samples, '--suffix', suffix, '-i', index])
+    result = runner.invoke(ps.printsample, ['-s', samples, '--replicates', '--suffix', suffix, '-i', index])
     assert result.exit_code == 0
-    ps.print_sample.assert_called_once_with(samples, suffix, index)
+    ps.print_sample.assert_called_once_with(samples, True, suffix, index)
 
 
 def test_printsample_samplesnotexists(testdir, mock_testclass):
@@ -70,9 +70,25 @@ def test_print_sample_first(testdir, capsys, mock_testclass):
 def test_print_sample_firstsuffix(testdir, capsys, mock_testclass):
     samples = Path(__file__).parent.joinpath('samples.txt')
     index = 0
-    ps.print_sample(samples, '-dedup', index)
+    ps.print_sample(samples, suffix='-dedup', index=index)
     captured = capsys.readouterr()
     assert captured.out == "POLR2A-dedup\n"
+
+
+def test_print_sample_firstreplicates(testdir, capsys, mock_testclass):
+    samples = Path(__file__).parent.joinpath('dataset.txt')
+    index = 0
+    ps.print_sample(samples, replicates=True, index=index)
+    captured = capsys.readouterr()
+    assert captured.out == "POLR2A\tPOLR2A_1\tPOLR2A_2\n"
+
+
+def test_print_sample_firstreplicatessuffix(testdir, capsys, mock_testclass):
+    samples = Path(__file__).parent.joinpath('dataset.txt')
+    index = 0
+    ps.print_sample(samples, replicates=True, suffix='-dedup', index=index)
+    captured = capsys.readouterr()
+    assert captured.out == "POLR2A-dedup\tPOLR2A_1-dedup\tPOLR2A_2-dedup\n"
 
 
 def test_print_sample_second(testdir, capsys, mock_testclass):
@@ -86,6 +102,22 @@ def test_print_sample_second(testdir, capsys, mock_testclass):
 def test_print_sample_secondsuffix(testdir, capsys, mock_testclass):
     samples = Path(__file__).parent.joinpath('samples.txt')
     index = 1
-    ps.print_sample(samples, '-dedup', index)
+    ps.print_sample(samples, suffix='-dedup', index=index)
     captured = capsys.readouterr()
     assert captured.out == "ASDURF-dedup\n"
+
+
+def test_print_sample_secondreplicates(testdir, capsys, mock_testclass):
+    samples = Path(__file__).parent.joinpath('dataset.txt')
+    index = 1
+    ps.print_sample(samples, replicates=True, index=index)
+    captured = capsys.readouterr()
+    assert captured.out == "ASDURF\tASDURF_1\tASDURF_2\n"
+
+
+def test_print_sample_secondreplicatessuffix(testdir, capsys, mock_testclass):
+    samples = Path(__file__).parent.joinpath('dataset.txt')
+    index = 1
+    ps.print_sample(samples, replicates=True, suffix='-dedup', index=index)
+    captured = capsys.readouterr()
+    assert captured.out == "ASDURF-dedup\tASDURF_1-dedup\tASDURF_2-dedup\n"
